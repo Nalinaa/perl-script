5cc6394e7eef642f9a44c8217c49f71cc993a9c1
Nalinaa <114544530+Nalinaa@users.noreply.github.com>
2023-12-01 11:45:31 +0530

    Create unless.pl

---
+++ unless.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $value = 25;
+
+unless ($value > 20)
+{
+print "$value is not greater than 20\n";
+}
+
+else
+{
+print "$value is greater than 20\n";
+}
